956d2ee4665e824b31f990cbbd29db67dcefa15d
eguller <enginguller@gmail.com>
2013-11-16 01:36:37 +0100

    expired cache count

--- app/cache/ItemCache.java
+++ app/cache/ItemCache.java
@@ -93,7 +93,7 @@ public class ItemCache {
             }
 
             itemList.removeAll(expiredItems);
-            Logger.info("%s items expired in cache and cleaned up");
+            Logger.info("%s items expired in cache and cleaned up", expiredItems.size());
         }
     }
 
