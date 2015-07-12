03a50dd7dd8cd444362157e931293fd668c150f4
eguller <enginguller@gmail.com>
2015-07-12 15:04:05 +0200

    send email via email chimp

--- app/utils/EmailUtil.java
+++ app/utils/EmailUtil.java
@@ -1,12 +1,19 @@
 package utils;
 
+import com.microtripit.mandrillapp.lutung.MandrillApi;
+import com.microtripit.mandrillapp.lutung.model.MandrillApiError;
+import com.microtripit.mandrillapp.lutung.view.MandrillMessage;
+import com.microtripit.mandrillapp.lutung.view.MandrillMessageStatus;
 import models.Subscription;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.HtmlEmail;
+import play.Logger;
 import play.libs.Mail;
 import play.templates.Template;
 import play.templates.TemplateLoader;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,7 +27,12 @@ public class EmailUtil {
     public static void sendEmail(String htmlContent, String textContent, String to, String subject) throws EmailException {
         sendEmail(htmlContent, textContent, to, subject, EMPTY_HEADER);
     }
-    public static void sendEmail(String htmlContent, String textContent, String to, String subject, Map<String, String> headers) throws EmailException {
+
+    public static void sendEmail(String htmlContent, String textContent, String to, String subject, Map<String, String> headers) throws EmailException{
+        sendMandrillAppMail(htmlContent, textContent, to, subject, headers);
+    }
+
+    private static void sendEmailPlay(String htmlContent, String textContent, String to, String subject, Map<String, String> headers) throws EmailException {
         HtmlEmail email = new HtmlEmail();
         email.addTo(to);
         email.setFrom("toplinks@hntoplinks.com", "Hacker News Top Links");
@@ -29,6 +41,37 @@ public class EmailUtil {
         email.setTextMsg(textContent);
         email.setHeaders(headers);
         Mail.send(email);
+    }
+
+    private static void sendMandrillAppMail(String htmlContent, String textContent, String to, String subject, Map<String, String> headers) {
+        String apiKey = System.getProperty("mandrillapi.key");
+        MandrillApi mandrillApi = new MandrillApi(apiKey);
+        MandrillMessage message = new MandrillMessage();
+        message.setSubject(subject);
+        message.setHtml(htmlContent);
+        message.setAutoText(true);
+        message.setFromEmail("toplinks@hntoplinks.com");
+        message.setFromName("Hacker News Top Links");
+        message.setHeaders(headers);
+        message.setText(textContent);
+        ArrayList<MandrillMessage.Recipient> recipients = new ArrayList<MandrillMessage.Recipient>();
+        MandrillMessage.Recipient recipient = new MandrillMessage.Recipient();
+        recipient.setEmail(to);
+        recipients.add(recipient);
+        message.setTo(recipients);
+        message.setPreserveRecipients(true);
+        try {
+            MandrillMessageStatus[] messageStatusReports = mandrillApi
+                    .messages().send(message, false);
+            for(MandrillMessageStatus status : messageStatusReports) {
+                Logger.info("Message sent. id: %s, email: %s, status: %s, rejection reason: %s",status.getId(), status.getEmail(), status.getStatus(), status.getRejectReason());
+            }
+        } catch (MandrillApiError mandrillApiError) {
+            mandrillApiError.printStackTrace();
+            Logger.error(mandrillApiError, "Sending email failed recipient: %s, subject: %s", to, subject);
+        } catch (IOException e) {
+            Logger.error(e, "Sending email failed recipient: %s, subject: %s", to, subject);
+        }
     }
 
     public static void sendActivationEmail(Subscription subscription, String to) throws EmailException {
