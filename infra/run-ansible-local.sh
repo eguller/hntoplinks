ac537c78d310ce40e6d4367c8491a2300769205a
eguller <enginguller@gmail.com>
2022-10-16 22:31:35 +0200

    deploy via ansible

--- infra/run-ansible-local.sh
+++ infra/run-ansible-local.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-
-ansible-playbook -i ansible/local.yml --private-key utils/fedora-ssh/.ssh/id_rsa ansible/site.yml
+cd "$(dirname "$0")"
+export POSTGRES_DB_USER_PASSWORD="postgres_dummy";
+export HNTOPLINKS_DB_USER_PASSWORD="hntoplinks_dummy";
+export SENDGRID_API_KEY="dummy_api_key";
+ansible-playbook -i ansible/local.yml --private-key utils/fedora-ssh/.vagrant/machines/default/virtualbox/private_key ansible/site.yml
