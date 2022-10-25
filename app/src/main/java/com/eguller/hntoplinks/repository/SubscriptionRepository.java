e826e7e0be25c2cfb6516aada3bd30c1aece78cb
eguller <enginguller [at] gmail.com>
2022-10-25 14:46:15 +0200

    update templates

--- app/src/main/java/com/eguller/hntoplinks/repository/SubscriptionRepository.java
+++ app/src/main/java/com/eguller/hntoplinks/repository/SubscriptionRepository.java
@@ -1,25 +1,20 @@
 package com.eguller.hntoplinks.repository;
 
-import com.eguller.hntoplinks.entities.SubscriberEntity;
 import com.eguller.hntoplinks.entities.SubscriptionEntity;
-import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface SubscriptionRepository extends CrudRepository<SubscriptionEntity, Long> {
   @Query("""
      select * from
-     SubscriptionEntity subscription
-     inner join Subscriber subscriber
+     subscription
+     inner join subscriber
      on subscription.subscriber = subscriber.id
      where
      subscriber.activated = true and
-     subscription.nextSendDate < now()
+     subscription.next_send_date < now()
     """)
   List<SubscriptionEntity> findSubscriptionsByExpiredNextSendDate();
 
