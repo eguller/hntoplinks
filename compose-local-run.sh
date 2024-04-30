ff1b190d25ebbc93fe3f17b8894b5dfab34c87e2
Engin Guller <enginguller@gmail.com>
2024-04-30 19:38:08 +0200

    spring boot 6

--- compose-local-run.sh
+++ compose-local-run.sh
@@ -1,11 +1,11 @@
 # /bin/sh
 
-
-basedir=$(dirname "$0")
+basedir="$(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")"
+echo $basedir
 gradle build
 cd app
 docker build -t hntoplinks:latest .
 cd $basedir
-docker-compose --project-name hntoplinks-app -f ${basedir}/infra/local/docker-compose-db.yml up -d
+docker-compose --project-name hntoplinks-app -f ${basedir}/infra/local/docker-compose.yml up -d
 
 echo "Local environment is up and running at http://localhost:8066"
