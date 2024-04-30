0cc624cc52c2fa4e7972996b254d48070aa00434
Engin Guller <enginguller@gmail.com>
2024-04-30 22:55:34 +0200

    thymeleaf update

--- app/src/main/java/com/eguller/hntoplinks/repository/StatisticRepository.java
+++ app/src/main/java/com/eguller/hntoplinks/repository/StatisticRepository.java
@@ -10,61 +10,81 @@ import java.util.List;
 
 @Repository
 public class StatisticRepository {
-  private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-  public StatisticRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-    this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
-  }
+    public StatisticRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
 
-  public void updateStatistic(StatKey statKey, String statValue) {
-    namedParameterJdbcTemplate.update("""
-        UPDATE
-         statistic
-        SET
-          stat_value = :statValue
-        WHERE
-          stat_key = :statKey
-          """,
-      new MapSqlParameterSource()
-        .addValue("statKey", statKey)
-        .addValue("statValue", statValue)
-    );
-  }
+    public void updateStatistic(StatKey statKey, String statValue) {
+        namedParameterJdbcTemplate.update("""
+                        UPDATE
+                         statistic
+                        SET
+                          stat_value = :statValue
+                        WHERE
+                          stat_key = :statKey
+                          """,
+                new MapSqlParameterSource()
+                        .addValue("statKey", statKey)
+                        .addValue("statValue", statValue)
+        );
+    }
 
-  public void saveAll(List<StatisticEntity> statisticEntityList) {
-    namedParameterJdbcTemplate.batchUpdate(
-      """
-        INSERT INTO statistic (stat_key, stat_value)
-        VALUES (:statKey, :statValue)
-        ON CONFLICT (stat_key)
-        DO
-          UPDATE
-            SET
-              stat_value = :statValue
-      """,
-      statisticEntityList.stream()
-        .map(statisticEntity -> new MapSqlParameterSource()
-          .addValue("statKey", statisticEntity.getStatKey())
-          .addValue("statValue", statisticEntity.getStatValue())
-        )
-        .toArray(MapSqlParameterSource[]::new)
-    );
-  }
+    public void saveAll(List<StatisticEntity> statisticEntityList) {
+        var entitiesWithId = statisticEntityList.stream()
+                .filter(statisticEntity -> statisticEntity.getId() != null)
+                .toList();
+        var entitiesWithoutId = statisticEntityList.stream().filter(statisticEntity -> statisticEntity.getId() == null).toList();
 
-  public List<StatisticEntity> findAll() {
-    var result = namedParameterJdbcTemplate.query(
-      """
-        SELECT
-         *
-        FROM
-          statistic
-      """,
-      (rs, rowNum) -> new StatisticEntity(
-        rs.getLong("id"),
-        rs.getString("stat_key"),
-        rs.getString("stat_value")
-      )
-    );
-    return result;
-  }
+        namedParameterJdbcTemplate.batchUpdate(
+                """
+                          INSERT INTO statistic (id, stat_key, stat_value)
+                          VALUES (nextval('hibernate_sequence'), :statKey, :statValue)
+                          ON CONFLICT (stat_key)
+                          DO
+                            UPDATE
+                              SET
+                                stat_value = :statValue
+                        """,
+                entitiesWithoutId.stream()
+                        .map(statisticEntity -> new MapSqlParameterSource()
+                                .addValue("statKey", statisticEntity.getStatKey())
+                                .addValue("statValue", statisticEntity.getStatValue())
+                        )
+                        .toArray(MapSqlParameterSource[]::new)
+        );
+
+        namedParameterJdbcTemplate.batchUpdate(
+                """
+                          UPDATE
+                            statistic
+                          SET
+                            stat_value = :value
+                          WHERE
+                            stat_key = :key
+                        """,
+                entitiesWithId.stream().map(statisticEntity -> new MapSqlParameterSource()
+                        .addValue("key", statisticEntity.getStatKey())
+                        .addValue("value", statisticEntity.getStatValue())
+                ).toArray(MapSqlParameterSource[]::new)
+        );
+    }
+
+    public List<StatisticEntity> findAll() {
+        var result = namedParameterJdbcTemplate.query(
+                """
+                          SELECT
+                           *
+                          FROM
+                            statistic
+                        """,
+                (rs, rowNum) -> new StatisticEntity(
+                        rs.getLong("id"),
+                        rs.getString("stat_key"),
+                        rs.getString("stat_value")
+                )
+        );
+        return result;
+    }
 }
