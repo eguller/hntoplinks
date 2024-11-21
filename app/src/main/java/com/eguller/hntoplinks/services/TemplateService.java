d786bf90973ea1855f52649394bacd7f64c8414a
Engin Guller <enginguller@gmail.com>
2024-11-21 01:39:21 +0100

    new ui and query all stories

--- app/src/main/java/com/eguller/hntoplinks/services/TemplateService.java
+++ app/src/main/java/com/eguller/hntoplinks/services/TemplateService.java
@@ -3,6 +3,7 @@ package com.eguller.hntoplinks.services;
 import java.util.List;
 import java.util.Locale;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,43 +22,55 @@ import lombok.Data;
 @Service
 public class TemplateService {
   @Autowired
-  @Qualifier("emailTemplateEngine") private TemplateEngine templateEngine;
+  @Qualifier("emailTemplateEngine")
+  private TemplateEngine templateEngine;
 
   @Value("${hntoplinks.base-url}")
   private String hntoplinksBaseUrl;
 
+  private Context ctx = new Context(Locale.ENGLISH);
+
+
+  @PostConstruct
+  public void init() {
+    //ctx.setVariable("fmt", );
+  }
+
   public String generateSubscriptionEmail(SubscriberEntity subscriber) {
     var periods =
-        subscriber.getSubscriptionList().stream().map(SubscriptionEntity::getPeriod).toList();
-    periods.sort(Period::compareTo);
+      subscriber.getSubscriptionList().stream().map(SubscriptionEntity::getPeriod).toList();
+    var sortedPeriods = periods.stream()
+      .sorted(Period::compareTo)
+      .toList();
 
     var subscriptionEmailData =
-        SubscriptionEmailData.builder()
-            .unsubscribeUrl(
-                "%s/subscribers/%s?action=unsubscribe"
-                    .formatted(hntoplinksBaseUrl, subscriber.getSubscriberId()))
-            .periods(periods)
-            .build();
+      SubscriptionEmailData.builder()
+        .baseUrl(hntoplinksBaseUrl)
+        .unsubscribeUrl(
+          "%s/subscribers/%s?action=unsubscribe"
+            .formatted(hntoplinksBaseUrl, subscriber.getSubscriberId()))
+        .periods(sortedPeriods)
+        .build();
     final Context ctx = new Context(Locale.ENGLISH);
     ctx.setVariable("data", subscriptionEmailData);
 
     final String htmlContent =
-        this.templateEngine.process("/email/html/subscription_email.html", ctx);
+      this.templateEngine.process("/email/html/subscription_email.html", ctx);
     return htmlContent;
   }
 
   public String generateTopEmail(
-      String subject, SubscriberEntity subscriber, List<Item> topEmails) {
+    String subject, SubscriberEntity subscriber, List<Item> topEmails) {
     var toplinksEmailData =
-        TopEmailData.builder()
-            .subject(subject)
-            .unsubscribeUrl(
-                "%s/subscribers/%s?action=unsubscribe"
-                    .formatted(hntoplinksBaseUrl, subscriber.getSubscriberId()))
-            .updateSubscriptionUrl(
-                "%s/subscribers/%s".formatted(hntoplinksBaseUrl, subscriber.getSubscriberId()))
-            .items(topEmails)
-            .build();
+      TopEmailData.builder()
+        .subject(subject)
+        .unsubscribeUrl(
+          "%s/subscribers/%s?action=unsubscribe"
+            .formatted(hntoplinksBaseUrl, subscriber.getSubscriberId()))
+        .updateSubscriptionUrl(
+          "%s/subscribers/%s".formatted(hntoplinksBaseUrl, subscriber.getSubscriberId()))
+        .items(topEmails)
+        .build();
 
     final Context ctx = new Context(Locale.ENGLISH);
     ctx.setVariable("data", toplinksEmailData);
@@ -68,17 +81,18 @@ public class TemplateService {
   @Builder
   @Data
   private static class TopEmailData {
-    private String subject;
-    private String baseUrl;
-    private String unsubscribeUrl;
-    private String updateSubscriptionUrl;
+    private String     subject;
+    private String     baseUrl;
+    private String     unsubscribeUrl;
+    private String     updateSubscriptionUrl;
     private List<Item> items;
   }
 
   @Builder
   @Data
   private static class SubscriptionEmailData {
-    private String unsubscribeUrl;
+    private String baseUrl;
+    private String       unsubscribeUrl;
     private List<Period> periods;
   }
 }
